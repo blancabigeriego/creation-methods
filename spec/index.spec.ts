ad36c889c828ec14e6d682dab5dd9efd2da029bf
DESKTOP-NCPM21O\Blanca <blanca.bigeriego@es.gestamp.com>
2024-07-01 19:07:41 +0200

    Test: add test to internet and tv selection

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -17,13 +17,13 @@ describe('ProductPackage', () => {
         expect(productPackage.hasTv()).toBe(false);
     });
 
-    // xit('should create a product package with internet and TV', () => {
-    //     const productPackage = new ProductPackage("100MB", ["LaLiga", "Estrenos"]);
+    it('should create a product package with internet and TV', () => {
+        const productPackage = ProductPackage.createWithInternetAndTv("100MB", ["LaLiga", "Estrenos"]);
 
-    //     expect(productPackage.hasInternet()).toBe(true);
-    //     expect(productPackage.hasVOIP()).toBe(false);
-    //     expect(productPackage.hasTv()).toBe(true);
-    // });
+        expect(productPackage.hasInternet()).toBe(true);
+        expect(productPackage.hasVOIP()).toBe(false);
+        expect(productPackage.hasTv()).toBe(true);
+    });
 
     // xit('should create a product package with internet, VoIP, and TV', () => {
     //     const productPackage = new ProductPackage("100MB", 91233788, ["LaLiga", "Estrenos"]);
